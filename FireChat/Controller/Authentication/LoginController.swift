7d139d4a3216c08019e6cc904d8fec67c97b72c2
MateuSales <matheussalles40@gmail.com>
2020-05-19 19:22:15 -0300

    Add extension UIView

--- FireChat/Controller/Authentication/LoginController.swift
+++ FireChat/Controller/Authentication/LoginController.swift
@@ -11,6 +11,14 @@ import UIKit
 class LoginController: UIViewController {
     // MARK: - Properties
     
+    private let imageIcon: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(systemName: "bubble.right")
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.tintColor = .white
+        return iv
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -26,6 +34,12 @@ class LoginController: UIViewController {
         navigationController?.navigationBar.barStyle = .black
         
         configureGradientLayer()
+        
+        view.addSubview(imageIcon)
+        imageIcon.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageIcon.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        imageIcon.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        imageIcon.widthAnchor.constraint(equalToConstant: 120).isActive = true
         
     }
     
