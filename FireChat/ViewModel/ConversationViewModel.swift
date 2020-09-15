3a80283b0d069bb6ae83bf38ce06cf3b7529fe95
MateuSales <matheussalles40@gmail.com>
2020-09-14 21:12:39 -0300

    create other feature

---
+++ FireChat/ViewModel/ConversationViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ConversationViewModel.swift
+//  FireChat
+//
+//  Created by Mateus Sales on 14/09/20.
+//  Copyright © 2020 Mateus Sales. All rights reserved.
+//
+
+import Foundation
+
+struct ConversationViewModel {
+    private let conversation: Conversation
+    
+    //var profileImageUrl: 
+}
